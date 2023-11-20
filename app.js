f1ddb6af06a1823c3b30680e3687a61056fa1ee5
Vinicius Ferreira Bezerra <vinicius.ferreira.bezerra07@aluno.ifce.edu.br>
2023-11-20 20:00:07 -0300

    commit de backup

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-let varia = 5
+// Link da API do Git https://api.github.com/users/a
+
+// let user = 
+const getProfile = () => {
+    const profile = fetch("https://api.github.com/users/Vinicius-Ferreira-Bezerra07")
+
+    profile
+        .then(resposta = res.json())
+        .then()
+}
