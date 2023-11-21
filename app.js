7e69b0739a0d11f57c815bf5f4129fb0921c96d8
Vinicius Ferreira Bezerra <vinicius.ferreira.bezerra07@aluno.ifce.edu.br>
2023-11-21 16:44:21 -0300

    backup

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
 // const getProfile = () => {
     const profile = fetch("https://api.github.com/users/joseolinda")
     console.log(1);
-    const li = document.createElement('li')
+    const ul = document.createElement('ul')
     
     profile
         .then(resposta => resposta.json())
@@ -18,12 +18,19 @@
             console.log(followers_urlGit);
             console.log(gists_urlGit);
 
+            const li = document.createElement('li')
             const avatar = document.createElement('img')
-            avatar.scr = avatar_urlGit
+            avatar.scr =  avatar_urlGit
+            
+
             li.appendChild(avatar)
+            ul.appendChild(li)
+
+            document.querySelector('.avatar').appendChild(ul)
         })
     
-    document.querySelector('#avatar').appendChild(li)
+   let x = document.querySelector('.avatar').appendChild(li)
+   x = `<a class="avatar" target="_top"><img src="${avatar_urlGit}" alt=""></a>`
 // }
 
 // const btnBuscar = document.querySelector('#buscar-github')
